e13500e8a765045c6cd13840099d93fb12a93cfb
Sebastian Dziura <sebastian_dziura@icloud.com>
2021-10-07 13:52:56 +0200

    add bird

--- main.py
+++ main.py
@@ -16,10 +16,26 @@ pygame.display.set_caption('Flappy Bird')
 #define game variables
 ground_scroll = 0
 scroll_speed = 3
+num = 1
 
 #load images
 bg_img = pygame.image.load('resources/bg.png', 'backgound')
 ground_img = pygame.image.load('resources/ground.png', 'ground')
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(f'resources/bird{num}.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
+
+
 
 run = True
 while run:
@@ -28,6 +44,9 @@ while run:
 
     #draw background
     screen.blit(bg_img, (0,0))
+
+    bird_group.draw(screen)
+
     #draw ground
     screen.blit(ground_img, (ground_scroll, 769))
     ground_scroll -= scroll_speed
